24997a44072e562dc799e7536c10db158266489d
wweiGit <15382353582@163.com>
2020-07-05 23:06:36 +0800

    add_14

---
+++ 14_longestCommonPrefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = ''
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                ans += i[0]
+            else:
+                break
+        return ans
+
+
